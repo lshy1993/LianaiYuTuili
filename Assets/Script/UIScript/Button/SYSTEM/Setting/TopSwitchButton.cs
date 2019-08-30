2780c6f755eaf3850d652622adb712960f817f25
lshy1993 <lshy1993@live.com>
2019-08-31 03:19:19 +0900

    demo 1.45b

--- Assets/Script/UIScript/Button/SYSTEM/Setting/TopSwitchButton.cs
+++ Assets/Script/UIScript/Button/SYSTEM/Setting/TopSwitchButton.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 public class TopSwitchButton : BasicButton
 {
     public UILabel hint;
+    public GraphicSettingUIManager uiManager;
 
     protected override void Hover(bool ishover)
     {
         hint.text = ishover ? "设置画面是否【处于最前】（该功能未实装）" : string.Empty;
+    }
+
+    protected override void Execute()
+    {
+        uiManager.SwitchTop();
     }
 }
--- Assets/Script/UIScript/OnClick/Click_Next.cs
+++ Assets/Script/UIScript/OnClick/Click_Next.cs
@@ -19,19 +19,20 @@ public class Click_Next : MonoBehaviour {
     void OnClick()
     {
         if (Input.GetMouseButtonUp(1)) return;
-        //如果锁定点击 则直接返回
+        // 如果锁定点击 则直接返回
         if (DataManager.GetInstance().IsClickBlocked())
         {
             Debug.Log("blocked");
             return;
         }
-        //如果auto模式开启 则重置计时器
+        // 如果auto模式开启 则关闭计时器
         else if (DataManager.GetInstance().isAuto)
         {
+            Execute();
             Debug.Log("刷新计时器");
             ta.ResetTimer();
         }
-        //如果对话框被隐藏
+        // 如果对话框被隐藏
         else if (uiManger.IsBoxClosed())
         {
             Debug.Log("显示对话框");
